4d77bb3ea0acfe618b4c343ded10226e827bdc31
pqwy <dk505@cam.ac.uk>
2020-04-02 18:03:27 +0100

    toplevel: pretty-print colors and attributes

--- src/notty_top_init.ml
+++ src/notty_top_init.ml
@@ -2,8 +2,16 @@
    See LICENSE.md. *)
 
 open Notty
-open Notty.Infix
 
-let pp_image_ansi = Render.pp Cap.ansi;;
+let pp_image_ansi = Render.pp Cap.ansi
+let pp_color_ansi =
+  let stencil = I.string "⏺" in
+  fun ppf c -> pp_image_ansi ppf (I.attr (A.fg c) stencil)
+let pp_attr_ansi =
+  let stencil = I.string "attr" in
+  fun ppf a -> pp_image_ansi ppf (I.attr a stencil)
+;;
 
 #install_printer pp_image_ansi
+#install_printer pp_color_ansi
+#install_printer pp_attr_ansi
