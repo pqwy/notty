77353e976483e11afa10864f7337fb0ae2508be8
Kate <kit.ty.kate@disroot.org>
2022-01-22 16:57:20 +0000

    Add support for OCaml 4.14

--- src/notty_top.ml
+++ src/notty_top.ml
@@ -1,4 +1,8 @@
 (* Copyright (c) 2017 David Kaloper Meršinjak. All rights reserved.
    See LICENSE.md. *)
 
+#if OCAML_VERSION >= (4,14,0)
+let _ = Toploop.use_silently Format.err_formatter (Toploop.File "notty_top_init.ml")
+#else
 let _ = Toploop.use_silently Format.err_formatter "notty_top_init.ml"
+#endif
